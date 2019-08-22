55b35a72d0a0902df0c15f83e66a94dfa1f6723b
Vladimír Jarabica <vladojarabica@gmail.com>
2019-08-22 10:00:17 +0200

    build

---
+++ build/precache-manifest.f0de3a08de5720099aa10ca62a1d3456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa68cee3ffbb8ab28b2fd363f42a1b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "882ac6a36a481b6f5c4f",
+    "url": "/static/css/main.685bf38f.chunk.css"
+  },
+  {
+    "revision": "d773c6fa15285a884290",
+    "url": "/static/js/2.e489b80f.chunk.js"
+  },
+  {
+    "revision": "882ac6a36a481b6f5c4f",
+    "url": "/static/js/main.554d15ed.chunk.js"
+  },
+  {
+    "revision": "f83765924c99157fd63c",
+    "url": "/static/js/runtime~main.dc7af181.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
